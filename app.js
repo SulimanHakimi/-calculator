5c8fb8de180fc7752e95952997932db8363a2fcf
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-01-22 10:28:29 -0800

    fix opration btns and add btn to screen

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 let output = document.querySelector(".output");
 let vals = document.querySelectorAll(".button");
-let equal = document.querySelector(".equal")
-let ac = document.querySelector(".ac")
-let delet = document.querySelector(".del")
-vals.forEach(val => {
-  val.addEventListener("click", ()=>{
-    console.log(output.value)
-  })
+let operations = document.querySelectorAll(".operation");
+let equal = document.querySelector(".equal");
+let ac = document.querySelector(".ac");
+let delet = document.querySelector(".del");
+
+vals.forEach((val) => {
+  val.addEventListener("click", () => {
+    output.value += val.value;
+    console.log(output.value);
+  });
 });
 
-ac.addEventListener("click",()=>{
-  output.value = "0"
-})
+ac.addEventListener("click", () => {
+  output.value = "";
+});
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    if (operation.value == "11") {
+      output.value += " " + "+" + " ";
+    } else if (operation.value == "12") {
+      output.value += " " + "-" + " ";
+    } else if (operation.value == "13") {
+      output.value += " " + "*" + " ";
+    } else if (operation.value == "14") {
+      output.value += " " + "/" + " ";
+    } else if (operation.value == "15") {
+      output.value += " " + "%" + " ";
+    }
+  });
+});
+
+equal.addEventListener("click", () => {
+  let result = eval(output.value);
+  output.value = result;
+});
